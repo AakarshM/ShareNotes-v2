b48f457fab589abdc9afdd5b7931a6be968b621d
AakarshM <a3madhav@edu.uwaterloo.ca>
2017-08-13 17:08:55 -0400

    getting work

--- Spring-server/src/main/java/com/sharenotes/spring/SharenotesSpringApplication.java
+++ Spring-server/src/main/java/com/sharenotes/spring/SharenotesSpringApplication.java
@@ -3,10 +3,28 @@ package com.sharenotes.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class SharenotesSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SharenotesSpringApplication.class, args);
+		try{
+
+			getConnection();
+			System.out.println("DB CONNED!");
+
+		} catch (Exception ex){
+
+		}
+	}
+
+	private static Connection getConnection() throws URISyntaxException, SQLException {
+		String dbUrl = System.getenv("JDBC_DATABASE_URL");
+		return DriverManager.getConnection(dbUrl);
 	}
 }
