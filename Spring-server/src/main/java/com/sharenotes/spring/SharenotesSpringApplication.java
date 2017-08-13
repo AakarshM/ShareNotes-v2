758f454d0cc75216d406deda912fc4439755c358
AakarshM <a3madhav@edu.uwaterloo.ca>
2017-08-13 16:43:41 -0400

    jars

---
+++ Spring-server/src/main/java/com/sharenotes/spring/SharenotesSpringApplication.java
@@ -0,0 +1,12 @@
+package com.sharenotes.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharenotesSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharenotesSpringApplication.class, args);
+	}
+}
