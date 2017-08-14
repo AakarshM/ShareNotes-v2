257fe9ea616261df58352e245cb9b669e08b0f64
AakarshM <a3madhav@edu.uwaterloo.ca>
2017-08-14 01:46:06 -0400

    added db config partially

--- Spring-server/src/main/java/com/sharenotes/spring/SharenotesSpringApplication.java
+++ Spring-server/src/main/java/com/sharenotes/spring/SharenotesSpringApplication.java
@@ -13,18 +13,6 @@ public class SharenotesSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SharenotesSpringApplication.class, args);
-		try{
 
-			getConnection();
-			System.out.println("DB CONNED!");
-
-		} catch (Exception ex){
-
-		}
-	}
-
-	private static Connection getConnection() throws URISyntaxException, SQLException {
-		String dbUrl = System.getenv("JDBC_DATABASE_URL");
-		return DriverManager.getConnection(dbUrl);
 	}
 }
